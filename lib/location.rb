994a43e01e600399b21db29009ccf88ee21aa88e
David Divilly <d.divilly@gmail.com>
2018-01-25 14:38:22 +0000

    Added update_curr_dir

--- lib/location.rb
+++ lib/location.rb
@@ -1,11 +1,22 @@
 class Location
 
-  attr_reader :curr_dir, :dx, :dy
+  attr_reader :curr_dir, :dx, :dy, :directions
 
-  def initialize
+  def initialize(directions = [])
     @curr_dir = 0
     @dx = 0
     @dy = 0
+    @directions = directions
   end
+
+  def update_curr_dir
+    @directions.each do | dir |
+      next if dir == 'forward'
+      dir == 'right' ? @curr_dir += 90 : @curr_dir -= 90
+    end
+  end
+
+
+
 
 end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -1,7 +1,8 @@
 require 'location'
 
 RSpec.describe Location do
-  subject(:location) { described_class.new }
+  let (:array) { %w[forward right] }
+  subject(:location) { described_class.new(array) }
 
   context '#initalize' do
     it 'should start with dir 0' do
@@ -16,6 +17,16 @@ RSpec.describe Location do
       expect(location.dy).to eq(0)
     end
 
-
+    it 'should should show the array' do
+      expect(location.directions).to eq(array)
+    end
   end
+
+  context '#update_curr_dir' do
+    it 'should update the curr direction' do
+      location.update_curr_dir
+      expect(location.curr_dir).to eq(90)
+    end
+  end
+
 end
