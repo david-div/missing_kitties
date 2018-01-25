6a7f75cef95ddfd9714b2133cb185213ad834323
David Divilly <d.divilly@gmail.com>
2018-01-25 14:01:42 +0000

    Added current dir, dx, dy

--- lib/location.rb
+++ lib/location.rb
@@ -1,1 +1,11 @@
+class Location
 
+  attr_reader :curr_dir, :dx, :dy
+
+  def initialize
+    @curr_dir = 0
+    @dx = 0
+    @dy = 0
+  end
+
+end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -1,11 +1,21 @@
-# require 'location'
-#
-# RSpec.describe Location do
-#
-#   subject(:location) { described_class.new }
-#
-#   context '#initalize' do
-#
-#   end
-#
-# end
+require 'location'
+
+RSpec.describe Location do
+  subject(:location) { described_class.new }
+
+  context '#initalize' do
+    it 'should start with dir 0' do
+      expect(location.curr_dir).to eq(0)
+    end
+
+    it 'should start with dir dx pos 0' do
+      expect(location.dx).to eq(0)
+    end
+
+    it 'should start with dir dy pos 0' do
+      expect(location.dy).to eq(0)
+    end
+
+
+  end
+end
