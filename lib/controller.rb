0475233a764f78dfd18081447ac2b0635dcec0c7
David Divilly <d.divilly@gmail.com>
2018-02-02 17:29:51 +0000

    Removed comments

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,15 +2,8 @@ require_relative 'get_data'
 require_relative 'location'
 require_relative 'directions'
 
-# get data is passed to Directions
-# directions is passed to location
-# returns the locaiton coordinates
-# it's then passed back to get data
-# get request is made but with the location url
-# coordinates are passed in
 
 class Controller
-
   attr_reader :data, :directions, :location
 
   def initialize(data = GetData, directions = Directions, location = Location)
@@ -31,16 +24,4 @@ class Controller
   def find_location
     location.new(get_directions).return_coordinates
   end
-
-
-
-
 end
-
-
-controller = Controller.new
-p controller.confirm_location
-
-#
-# then when these are passed, pass in ClassName.new in the method
-# need to be able to psss in a simple array
